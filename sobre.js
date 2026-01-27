5932859941deacaeea1c360db4095f2cf75d2fb1
unknown <hugokkaua@gmail.com>
2026-01-27 13:32:42 -0300

    melhoras-na-sideBarr

---
+++ sobre.js
@@ -0,0 +1,12 @@
+let Botaomenu = document.getElementById("BotaoMenu")
+
+
+function AtivarMenu() {
+    let menulateral = document.getElementById("menus")
+
+    addEventListener("click", function () {
+        menulateral.classList.toggle("aparecer")
+    })
+
+
+}

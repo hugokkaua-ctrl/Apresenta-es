952fd007f6132a0bebc0b60622800ed78b652927
unknown <hugokkaua@gmail.com>
2026-03-27 15:24:00 -0300

    Data

--- Project/Clima.js
+++ Project/Clima.js
@@ -31,14 +31,26 @@ function Horas() {
     setInterval(function () {
         let data = new Date()
 
+        /*HORARIO */
         let horas = data.getHours()
         let min = data.getMinutes()
         let sec = data.getSeconds()
+
+        /*DATA */
+        let mes = data.getMonth()
+        let dia = data.getDate()
+        let ano = data.getFullYear()
 
         if (horas < 10) {
             document.getElementById("hora").innerHTML = "0" + horas + ":" + min + ":" + sec
         } else {
             document.getElementById("hora").innerHTML = horas + ":" + min + ":" + sec
+        }
+
+        if (mes < 10 || dia < 10) {
+            document.getElementById("data").innerHTML = dia + "/0" + mes + "/" + ano
+        } else {
+            document.getElementById("data").innerHTML = dia + mes + "/" + "/" + ano
         }
 
     }, 1000)

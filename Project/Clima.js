780a83892b9a7ef84cabe7443683c850862ce60a
unknown <hugokkaua@gmail.com>
2026-03-29 08:21:56 -0300

    mudando-js

--- Project/Clima.js
+++ Project/Clima.js
@@ -1,31 +1,16 @@
-let inpus = document.getElementById("cidade").value
-let condicao = document.getElementById("condicao").innerHTML
+const inpus = document.getElementById("cidade")
 
-function enviar() {
-    let inpus = document.getElementById("cidade").value
-    let mod = inpus.toUpperCase()
+const city = inpus.value
 
-    document.getElementById("local").innerHTML = mod
-}
+const condicao = document.getElementById("condicao")
+const temperatura = document.getElementById("temperatura")
+const local = document.getElementById("local")
 
-async function enviar() {
-    const inpu = document.getElementById("cidade").value
 
-    const apiKey = "f25b9a544e6921f2164ae73c8435aed4"
 
-    try {
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inpu}&appid=${apiKey}&units=metric&lang=pt_br`);
-        const dados = await res.json();
 
-        document.getElementById("local").innerHTML = ` ${dados.name} `;
-        document.getElementById("temperatura").innerHTML = ` ${dados.main.temp}°C `;
-        document.getElementById("condicao").innerHTML = `  ${dados.weather[0].description}`;
 
-    } catch {
-        local.innerHTML = "Erro ao buscar clima";
-    }
 
-}
 
 function Horas() {
     setInterval(function () {

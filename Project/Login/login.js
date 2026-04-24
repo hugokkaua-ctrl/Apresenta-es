8270b6c0e58ad0d96d903b04d3ef434e92df6cfb
unknown <hugokkaua@gmail.com>
2026-04-24 15:28:25 -0300

    temas-funcionais

--- Project/Login/login.js
+++ Project/Login/login.js
@@ -1,5 +1,21 @@
+const area = document.getElementById("area")
 
+const btn1 = document.getElementById("claro")
+const btn2 = document.getElementById("dark")
 
 function envia() { }
 
 function submit() { }
+
+
+function mudar_dark() {
+    document.body.style.background = "#200F07"
+    area.style.background = "#702806"
+    btn1.style.display = "none"
+    btn2.style.display = "block"
+}
+function mudar_claro() {
+    document.body.style.background = "#C5E284"
+    btn1.style.display = "block"
+    btn2.style.display = "none"
+}

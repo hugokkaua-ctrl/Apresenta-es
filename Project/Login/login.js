632a005d573263e653b36f2a655bae2088bfb7b4
unknown <hugokkaua@gmail.com>
2026-04-25 11:13:12 -0300

    quase-finalizando-temas

--- Project/Login/login.js
+++ Project/Login/login.js
@@ -1,5 +1,7 @@
 const area = document.getElementById("area")
-
+const label1 = document.getElementById("label1")
+const label2 = document.getElementById("label2")
+const titulo = document.getElementById("title")
 const btn1 = document.getElementById("claro")
 const btn2 = document.getElementById("dark")
 
@@ -10,6 +12,9 @@ function submit() { }
 
 function mudar_dark() {
     document.body.style.background = "#200F07"
+    titulo.style.color = "#C5E284"
+    label1.style.color = "#C5E284"
+    label2.style.color = "#C5E284"
     area.style.background = "#702806"
     btn1.style.display = "none"
     btn2.style.display = "block"

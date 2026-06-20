72c9a55297695af93b0e13fa35db7cf51132f968
unknown <hugokkaua@gmail.com>
2026-06-20 12:24:00 -0300

    tempo-e-dia

--- Project/Login/home.js
+++ Project/Login/home.js
@@ -1,7 +1,21 @@
 const tempo = document.getElementById("tempo")
 
 function abrir() {
-    setInterval(() => {
 
+    const horas = document.getElementById("horas")
+    const dias = document.getElementById("dias")
+
+    setInterval(() => {
+        const data = new Date()
+
+        const hora = data.getHours()
+        const minuto = data.getMinutes()
+        const segundo = data.getSeconds()
+
+        const semana = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabádo"]
+        const semanas = semana[data.getDay()]
+
+        horas.innerHTML = hora + ":" + minuto + ":" + segundo
+        dias.innerHTML = semanas
     }, 1000)
 }

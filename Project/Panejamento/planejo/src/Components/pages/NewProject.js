3a086b38d5aa4df450b0fa266ca7198aae86dfeb
unknown <hugokkaua@gmail.com>
2026-07-20 07:04:16 -0300

    cria-planos-em planejamento

--- Project/Panejamento/planejo/src/Components/pages/NewProject.js
+++ Project/Panejamento/planejo/src/Components/pages/NewProject.js
@@ -2,16 +2,31 @@ import Project from "../Projects/Project"
 import { useNavigate } from "react-router-dom"
 
 function NewProject() {
-
+    const navigate = useNavigate()
 
     function createPost(project) {
+        project.cost = 0
+        project.service = []
 
+        fetch(`http://localhost:5000/project`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(project)
+        })
+
+            .then((resp) => resp.json())
+            .then((data) => {
+                navigate("/Projetos", { state: { message: "criado com sucesso" } })
+            })
+            .catch((err) => console.log(err))
     }
 
     return (
         <div>
             <h1>Façã seu agendamento dos aperitivos</h1>
-            <Project />
+            <Project handleSubmit={createPost} btnText="Enviar" />
         </div>
     )
 }
